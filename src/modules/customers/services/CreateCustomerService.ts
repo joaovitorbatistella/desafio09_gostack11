0cf0942c2dfb5dd2b4992b70ae1efe2bdf9bc4f5
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-07-05 16:38:08 -0300

    second commit

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const oldCustomer = await this.customersRepository.findByEmail(email);
+
+    if (oldCustomer) {
+      throw new AppError('E-mail already registered');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
